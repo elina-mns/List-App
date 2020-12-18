3d8ac4b34165917563e1d9efb4b111e426e962ab
elina-mns <ellie.mansurova@gmail.com>
2020-12-18 17:32:54 -0500

    implement deletion of categories by swiping to the left

--- List App/ViewControllers/CategoryViewController.swift
+++ List App/ViewControllers/CategoryViewController.swift
@@ -93,18 +93,21 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         guard orientation == .right else { return nil }
             let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
                 // handle action by updating model with deletion
+                
+                if let categoryForDeletion = self.categories?[indexPath.row] {
+                    do {
+                        try self.realm.write {
+                            self.realm.delete(categoryForDeletion)
+                        }
+                    } catch {
+                        print("Error deleting category \(error)")
+                    }
+                    tableView.reloadData()
+                }
             }
 
             // customize the action appearance
             deleteAction.image = UIImage(named: "delete")
             return [deleteAction]
     }
-    
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
-        var options = SwipeOptions()
-        options.expansionStyle = .destructive
-        options.transitionStyle = .border
-        return options
-    }
-
 }
